f397e72f422fe4f1b53609e0524d8ee3a2113e40
annLazore <117918263+annLazore@users.noreply.github.com>
2024-12-23 00:24:31 +0200

    Add files via upload
    PA3

--- WebGL-main/main.js
+++ WebGL-main/main.js
@@ -4,6 +4,9 @@ let gl;                         // The webgl context.
 let surface;                    // A surface model
 let shProgram;                  // A shader program
 let spaceball;                  // A SimpleRotator object that lets the user rotate the view by mouse.
+let normalTexture; // Оголошуємо змінну глобально
+let diffuseTexture;
+let roughnessTexture;
 
 function deg2rad(angle) {
     return angle * Math.PI / 180;
@@ -11,41 +14,13 @@ function deg2rad(angle) {
 
 
 // Constructor
-/*function Model(name) {
-    this.name = name;
-    this.iVertexBuffer = gl.createBuffer();
-    this.count = 0;
-
-    this.BufferData = function(vertices) {
-
-        gl.bindBuffer(gl.ARRAY_BUFFER, this.iVertexBuffer);
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STREAM_DRAW);
-
-        this.count = vertices.length/3;
-    }
-
-    this.Draw = function() {
-
-        gl.bindBuffer(gl.ARRAY_BUFFER, this.iVertexBuffer);
-        gl.vertexAttribPointer(shProgram.iAttribVertex, 3, gl.FLOAT, false, 0, 0);
-        gl.enableVertexAttribArray(shProgram.iAttribVertex);
-   
-        gl.drawArrays(gl.LINE_STRIP, 0, this.count);
-    }
-}*/
-
-
-// Constructor
 function ShaderProgram(name, program) {
 
     this.name = name;
     this.prog = program;
-
-    // Location of the attribute variable in the shader program.
     this.iAttribVertex = -1;
-    // Location of the uniform specifying a color for the primitive.
+    this.iAttribNormal = -1;
     this.iColor = -1;
-    // Location of the uniform matrix representing the combined transformation.
     this.iModelViewProjectionMatrix = -1;
 
     this.Use = function() {
@@ -58,74 +33,93 @@ function ShaderProgram(name, program) {
  * (Note that the use of the above drawPrimitive function is not an efficient
  * way to draw with WebGL.  Here, the geometry is so simple that it doesn't matter.)
  */
-function draw() { 
-    gl.clearColor(0,0,0,1);
+let time = 0; // Глобальна змінна для часу
+let lastTimestamp = 0; // Час останнього оновлення
+
+function updateLightPosition(deltaTime) {
+    // Розраховуємо нову позицію світла
+    const radius = 100.0;
+    const speed = 1.0;
+    time += deltaTime * speed;
+    return [
+        radius * Math.cos(time),       // X
+        5.0  ,    // Y обертається з амплітудою
+        radius * Math.sin(time)        // Z
+    ];
+    return [55.0, 50.0, 54.0];
+}
+
+function render(timestamp) {
+    // Розрахунок часу між кадрами
+    const deltaTime = (timestamp - lastTimestamp) / 1000; // У секундах
+    lastTimestamp = timestamp;
+
+    // Оновлюємо положення світла
+    const lightPosition = updateLightPosition(deltaTime);
+
+    // Передаємо позицію світла у шейдер
+    gl.uniform3fv(shProgram.lightPositionLocation, lightPosition);
+    gl.uniform3fv(shProgram.cameraPositionLocation, [0, 0, 10]); // Камера над об'єктом
+    gl.uniform4fv(shProgram.iColor, [0.5, 0.1, 1.0, 1.0]); // Базовий колір об'єкта
+
+
+    // Малюємо сцену
+    draw();
+
+    // Наступний кадр
+    requestAnimationFrame(render);
+}
+
+function draw() {
+    gl.clearColor(0, 0, 0, 1);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-    
-    /* Set the values of the projection transformation */
-    let projection = m4.perspective(Math.PI/8, 1, 8, 12); 
-    
-    /* Get the view matrix from the SimpleRotator object.*/
-    let modelView = spaceball.getViewMatrix();
 
-    let rotateToPointZero = m4.axisRotation([0.707,0.707,0], 0.7);
-    let translateToPointZero = m4.translation(0,0,-10);
+    // Diffuse texture
+    gl.activeTexture(gl.TEXTURE0);
+    gl.bindTexture(gl.TEXTURE_2D, diffuseTexture);
+    gl.uniform1i(shProgram.diffuseTextureLocation, 0);
 
-    let matAccum0 = m4.multiply(rotateToPointZero, modelView );
-    let matAccum1 = m4.multiply(translateToPointZero, matAccum0 );
-        
-    /* Multiply the projection matrix times the modelview matrix to give the
-       combined transformation matrix, and send that to the shader program. */
-    let modelViewProjection = m4.multiply(projection, matAccum1 );
+    // Normal texture
+    gl.activeTexture(gl.TEXTURE1);
+    gl.bindTexture(gl.TEXTURE_2D, normalTexture);
+    gl.uniform1i(shProgram.normalTextureLocation, 1);
 
-    gl.uniformMatrix4fv(shProgram.iModelViewProjectionMatrix, false, modelViewProjection );
-    
-    /* Draw the six faces of a cube, with different colors. */
-    gl.uniform4fv(shProgram.iColor, [1,1,0,1] );
+    // Roughness texture
+    gl.activeTexture(gl.TEXTURE2);
+    gl.bindTexture(gl.TEXTURE_2D, roughnessTexture);
+    gl.uniform1i(shProgram.roughnessTextureLocation, 2);
+
+    // Отримуємо трансформаційні матриці
+    const projection = m4.perspective(Math.PI / 8, 1, 8, 12);
+    const modelView = spaceball.getViewMatrix();
+
+    let rotateToPointZero = m4.axisRotation([0.707, 0.707, 0], 0.7);
+    let translateToPointZero = m4.translation(0, 0, -10);
+
+    let matAccum0 = m4.multiply(rotateToPointZero, modelView);
+    let matAccum1 = m4.multiply(translateToPointZero, matAccum0);
+    let modelViewProjection = m4.multiply(projection, matAccum1);
+
+    gl.uniformMatrix4fv(shProgram.iModelViewProjectionMatrix, false, modelViewProjection);
+
+    const modelMatrix = m4.identity();
+    m4.multiply(modelView, matAccum1, modelMatrix);
+    m4.inverse(modelMatrix, modelMatrix);
+    m4.transpose(modelMatrix, modelMatrix);
+    gl.uniformMatrix4fv(shProgram.modelMatrixLocation, false, modelMatrix);
+
+    // Передаємо текстурні координати
+    if (shProgram.iTexCoord >= 0) {
+        gl.bindBuffer(gl.ARRAY_BUFFER, surface.texCoordBuffer);
+        gl.vertexAttribPointer(shProgram.iTexCoord, 2, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(shProgram.iTexCoord);
+    }
 
     surface.Draw();
 }
 
-// function CreateSurfaceData()
-// {
-//     let vertexList = [];
-
-//     for (let i=0; i<360; i+=5) {
-//         vertexList.push( Math.sin(deg2rad(i)), 1, Math.cos(deg2rad(i)) );
-//         vertexList.push( Math.sin(deg2rad(i)), 0, Math.cos(deg2rad(i)) );
-//     }
-
-//     return vertexList;
-// }
 
 
-
-/* Initialize the WebGL context. Called from init() */
-/*function initGL() {
-    let prog = createProgram( gl, vertexShaderSource, fragmentShaderSource );
-
-    shProgram = new ShaderProgram('Basic', prog);
-    shProgram.Use();
-
-    shProgram.iAttribVertex              = gl.getAttribLocation(prog, "vertex");
-    shProgram.iModelViewProjectionMatrix = gl.getUniformLocation(prog, "ModelViewProjectionMatrix");
-    shProgram.iColor                     = gl.getUniformLocation(prog, "color");
-
-    surface = new Model('Surface');
-    surface.BufferData(CreateSurfaceData());
-
-    gl.enable(gl.DEPTH_TEST);
-}*/
-
-
-/* Creates a program for use in the WebGL context gl, and returns the
- * identifier for that program.  If an error occurs while compiling or
- * linking the program, an exception of type Error is thrown.  The error
- * string contains the compilation or linking error.  If no error occurs,
- * the program identifier is the return value of the function.
- * The second and third parameters are strings that contain the
- * source code for the vertex shader and for the fragment shader.
- */
 function createProgram(gl, vShader, fShader) {
     let vsh = gl.createShader( gl.VERTEX_SHADER );
     gl.shaderSource(vsh,vShader);
@@ -150,9 +144,6 @@ function createProgram(gl, vShader, fShader) {
 }
 
 
-/**
- * initialization function that will be called when the page has loaded
- */
 function init() {
     let canvas;
     try {
@@ -178,5 +169,10 @@ function init() {
 
     spaceball = new TrackballRotator(canvas, draw, 0);
 
-    draw();
+    document.getElementById('uGranularity').addEventListener('input', updateGranularity);
+    document.getElementById('vGranularity').addEventListener('input', updateGranularity);
+
+
+    // draw();
+    requestAnimationFrame(render);
 }
